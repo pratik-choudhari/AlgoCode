0b53b9a8fa1b500df0559b8bf1c068ce41452ab5
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-10-01 12:01:20 -0400

    feat: complete simple calculator with 4 basic operations

---
+++ Python/calculator/main.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def calculator(left: int, right: int, operation: str) -> None:
+  try:
+    result = {
+      '+': add,
+      '-': rest,
+      '/': _div,
+      '*': mul
+    }[operation](left, right)
+
+    print(result)
+  except Exception as _:
+    print(f'Operation ({operation}) is invalid.')
+
+def add(left: int, right: int) -> int:
+  return left + right
+
+def rest(left: int, right: int) -> int:
+  return left - right
+
+def _div(left: int, right: int) -> int:
+  return left / right
+
+def mul(left: int, right: int) -> int:
+  return left * right
+
+
+if __name__ == '__main__':
+  left, operation, right = sys.argv[1:]
+  
+  calculator(int(left), int(right), operation)
