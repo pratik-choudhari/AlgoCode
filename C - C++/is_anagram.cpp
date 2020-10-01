4cf1cc48c92d02ada2d4e87320a795e66fbee0bd
Utkarsh Bajaj <44445191+utkarshbajaj@users.noreply.github.com>
2020-10-01 16:34:24 +0530

    Adding is anagram for cpp

---
+++ C - C++/is_anagram.cpp
@@ -0,0 +1,40 @@
+/*	Language - cpp 
+	This code takes 2 strings as inputs and tells
+	is they are anagrams 
+	Note: all letters must be Lowercase/Uppercase */
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool isAnagram(string a, string b){
+
+	// Logic - After sorting if the two strings are equal, they are anagrams. 
+
+	// Sort stl in the algorithms header file is used for sorting. 
+	// Sorting of a strings means alphabetically, i.e. bcda after sorting become abcd 
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+
+	return a == b;
+}
+
+int main()
+{
+	/* code */
+	string a, b;
+	cout << "Enter the two strings\n";
+	// Taking input
+	cin >> a >> b;
+
+	// Function returns true is the strings are anagrams. 
+	if(isAnagram(a, b)){
+		cout << a << " & " << b << " are anagrams.";
+	} else {
+		cout << a << " & " << b << " are NOT anagrams.";
+	}
+	return 0;
+}
+
+// Author - Utkarsh Bajaj
