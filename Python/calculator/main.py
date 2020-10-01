396e6fc3144054ebf48aa7c7f7ca2fcadf279077
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-10-01 12:02:09 -0400

    update error message

--- Python/calculator/main.py
+++ Python/calculator/main.py
@@ -12,7 +12,7 @@ def calculator(left: int, right: int, operation: str) -> None:
 
     print(result)
   except Exception as _:
-    print(f'Operation ({operation}) is invalid.')
+    print(f'Operation: ({operation}) is invalid.')
 
 def add(left: int, right: int) -> int:
   return left + right
