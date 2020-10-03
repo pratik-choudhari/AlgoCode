234116f8d1f9a1653f857308eb4b070a8345fb0c
Om Uparkar <omauparkar1997@gmail.com>
2020-10-03 18:43:37 +0530

    :sparkles: Feature: A simple calculator in javascript

---
+++ Javascript/Calculator/app.js
@@ -0,0 +1,50 @@
+const prompt = require('prompt');
+
+const properties = [
+    {
+        name: 'firstInteger',
+        validator: /[\d]/,
+        warning: 'Input should only be an integer'
+    },
+    {
+        name: 'secondInteger',
+        validator: /[\d]/,
+        warning: 'Input should only be an integer'
+    },
+    {
+        name: 'operator',
+        validator: /[+\-\*\/\%]/,
+        warning: 'Operator should be either of these: +,-,*,/,%'
+    }
+];
+
+prompt.start();
+
+prompt.get(properties, function (error, result) {
+    if (error) {
+        return onError(error);
+    }
+    console.log("Commandline inputs received");
+    console.log("First input: " + result.firstInteger);
+    console.log("Second input: " + result.secondInteger);
+    console.log("Operator: " + result.operator);
+    console.log("Answer is");
+    if (result.operator === '+') {
+        return console.log(result.firstInteger + " + " + result.secondInteger + " = " + parseInt(result.firstInteger) + parseInt(result.secondInteger));
+    } else if (result.operator === '-') {
+        return console.log(result.firstInteger + " - " + result.secondInteger + " = " + (parseInt(result.firstInteger) - parseInt(result.secondInteger)));
+    } else if (result.operator === '*') {
+        return console.log(result.firstInteger + " * " + result.secondInteger + " = " + parseInt(result.firstInteger) * parseInt(result.secondInteger));
+    } else if (result.operator === '/') {
+        return console.log(result.firstInteger + " / " + result.secondInteger + " = " + parseInt(result.firstInteger) / parseInt(result.secondInteger));
+    } else if (result.operator === '%') {
+        return console.log(result.firstInteger + " % " + result.secondInteger + " = " + parseInt(result.firstInteger) % parseInt(result.secondInteger));
+    } else {
+        console.log("Something went wrong, cannot compute answer");
+    }
+});
+
+function onError(error) {
+    console.log(error);
+    return 1;
+}
