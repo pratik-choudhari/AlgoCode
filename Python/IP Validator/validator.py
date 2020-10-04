e3fab2d8e4f38296b927aab196fd657f2eeab8b6
Sudipto Ghosh <sudipto@ghosh.pro>
2020-10-04 09:43:19 +0530

    added screenshot and comments to IP validator

--- Python/IP Validator/validator.py
+++ Python/IP Validator/validator.py
@@ -1,11 +1,15 @@
 import re
 
 ipList = []
+
+# Accept user input
 n = int(input())
 for i in range(n):
     ip = ipList.append(input())
 
+# Iterate over IP addresses
 for ip in ipList:
+    # Check if any IP address matched the RegEx
     if (re.search(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', ip)):
         print('true')
     else:
