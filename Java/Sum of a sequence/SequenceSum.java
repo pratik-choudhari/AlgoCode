323f1e53a51a570cced1873370a397dd70b6bcd0
Aman Raj <64035554+amanraj862@users.noreply.github.com>
2020-10-04 15:18:22 +0530

    update documentation

--- Java/Sum of a sequence/SequenceSum.java
+++ Java/Sum of a sequence/SequenceSum.java
@@ -3,16 +3,16 @@ public class SequenceSum {
 	public static void main(String[] args) {
 		Scanner sn=new Scanner(System.in);
 		System.out.println("Enter Sequence limit");
-		int n=sn.nextInt();
+		int n=sn.nextInt();//taking limit of sequence as an input
 		int sum=0;
 	  System.out.print("Sequence: ");
 		for(int i=1;i<=n;i++) {
-			int term=(i*i)-(i-1)*(i-1);
-			System.out.print(term+" ");
-			sum=sum+term;
+			int term=(i*i)-(i-1)*(i-1);//calculating each term of sequence
+			System.out.print(term+" ");//printing elements of sequence
+			sum=sum+term;//calculate sum of sequence
 		}
 		System.out.println();
-		System.out.println("Sum: "+sum);
+		System.out.println("Sum: "+sum);//printing sum of sequence
 
 	}
 
