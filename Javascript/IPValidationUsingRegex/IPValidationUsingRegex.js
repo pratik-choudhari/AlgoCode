f57b9eeb0a0076bdc563620611f487a15ef7d9f1
jyoti <jshindems@gmail.com>
2020-10-04 20:56:32 -0400

    refactored code

--- Javascript/IPValidationUsingRegex/IPValidationUsingRegex.js
+++ Javascript/IPValidationUsingRegex/IPValidationUsingRegex.js
@@ -1,16 +1,14 @@
 //Ip Validation using regex
 function isValidIpv4Addr(ip) {
+    //define regex
     let regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+    //for eah ip address return true if it is valid IP address else return false
 	for (let i = 0; i <= ip.length; i++) {
-		if (
-			regex.test(
-				ip
-			)
-		) {
+		if (regex.test(ip)) {
 			return true;
+        }
+        return false;
     }
-    return false;
-}
 }
 
 console.log(isValidIpv4Addr('000.12.12.034'));
