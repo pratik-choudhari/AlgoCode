53fe7bbc0390f90ab143a5f24fcc976a9fbacea1
A Vamsi Mudaliar <43962195+VamsiMudaliar@users.noreply.github.com>
2020-10-05 18:25:02 +0530

    Update recursive.cpp

--- C - C++/Generate all binary strings from given pattern/recursive.cpp
+++ C - C++/Generate all binary strings from given pattern/recursive.cpp
@@ -1,3 +1,4 @@
+/* Language  C++ 17  */
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -5,18 +6,18 @@ using namespace std;
 void solve(string s,int idx){
 
     if(idx==s.length()){
-        cout<<s<<endl;
+        cout<<s<<endl; // if all the '?' have been matched we have found one combination
         return;
     }
     if(s[idx]=='?')
     {
-        s[idx]='0';
-        solve(s,idx+1);
-        s[idx]='1';
-        solve(s,idx+1);
+        s[idx]='0';  // try '?'----> '0'
+        solve(s,idx+1); // apply recursion for the rest of the string
+        s[idx]='1'; // try '?' ---> '1'
+        solve(s,idx+1); // apply recursion for the rest of string
     }
     else{
-        solve(s,idx+1);
+        solve(s,idx+1); // if the input is 0 or 1 then just skip and go to next index
     }
 
 }
@@ -25,8 +26,8 @@ void solve(string s,int idx){
 int main(){
 
     string s;
-    cin>>s;
+    cin>>s; // take the input
 
-    solve(s,0);
+    solve(s,0); // helper function
 
 }
