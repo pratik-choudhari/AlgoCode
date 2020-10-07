0c84811e1e199cb06e25e3d0caf7a254046b869a
Saurabh Daware <saurabhdaware2001@gmail.com>
2020-10-07 11:16:32 +0530

    Added DataTypes.java

---
+++ Java/Fundamentals/DataTypes.java
@@ -0,0 +1,38 @@
+package fundamentals;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		//Primary Data types
+		
+		//byte stores Integers form -128 to 127
+		byte a = 127;
+		
+		//short stores Integers from -32,768 to 32,767
+		short b = 32767;
+		
+		
+		//int stores Integers from - 2,147,483,648 to 2,147,483,647
+		int c = 2147483647;
+		
+		// long stores Integers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+		long d = 9223372036854775806L;
+		
+		// char stores a single character
+		char h = 's';
+		
+		// float stores decimal numbers
+		float pi = 3.14f;
+		
+		//Double stores large decimal numbers 
+		double e = 2.7182818284590452353602874713527;
+		
+		//boolean stores only 2 values either true or false
+		boolean f = false;
+		
+		// String stores a bunch of characters
+		String s = "Hello World";
+	}
+
+}
