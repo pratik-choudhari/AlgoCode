bba0ea2096b8fdb61c2eb63da97e2534b10ecb9b
ddaniss <danielssocas@gmail.com>
2020-10-12 09:29:38 +0100

    Add contribution requirements

---
+++ Javascript/PascalTriangle/README.md
@@ -0,0 +1,14 @@
+This code takes in a number of rows as input in the console.
+
+Then the program produces a Pascal's Triangle with a number of rows equal to the input entered by the user. 
+
+The program uses a for loop nested inside another for loop to produce an array of arrays.
+
+Sample Input: 
+3
+
+Sample Output: 
+[ 1 ]
+[ 1, 1 ]
+[ 1, 2, 1 ]
+
--- Javascript/PascalTriangle/pascalTriangle.js
+++ Javascript/PascalTriangle/pascalTriangle.js
@@ -15,15 +15,18 @@ rl.question("Enter the number of rows of the Pascal's Triangle: ", answer => {
     for (let i = 0; i < numRows; i++) {
       const row = new Array(i + 1);
 
+      // In each row we know the first and last number are both 1
       row[0] = 1;
       row[row.length - 1] = 1;
 
+      // Then we fill the rest of the row, doing the summations of the previous row
       for (let j = 1; j < row.length - 1; j++) {
         let rowAbove = pascalTriangle[i - 1];
         row[j] = rowAbove[j] + rowAbove[j - 1];
       }
 
       pascalTriangle[i] = row;
+      // Finally we print each row
       console.log(pascalTriangle[i]);
     }
     return pascalTriangle;
