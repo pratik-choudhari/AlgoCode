2757d28892147651dbeadf4908b9c8ac25320905
sripaad <sripaad751@gmail.com>
2020-10-13 15:20:40 +0530

    Added Morse code encoder decoder, hacktoberfest pr

---
+++ Python/Morse-Encode-Decode/README.md
@@ -0,0 +1,8 @@
+## About 
+> A Program to encode and decode Morse Code.
+
+
+@author : [Sripaad Srinivasan](https://github.com/Sripaad)
+
+## Run
+``` python3 main.py```
---
+++ Python/Morse-Encode-Decode/morse_encode_decode.py
@@ -0,0 +1,77 @@
+
+morse_alphabets ={
+        "A" : ".-",
+        "B" : "-...",
+        "C" : "-.-.",
+        "D" : "-..",
+        "E" : ".",
+        "F" : "..-.",
+        "G" : "--.",
+        "H" : "....",
+        "I" : "..",
+        "J" : ".---",
+        "K" : "-.-",
+        "L" : ".-..",
+        "M" : "--",
+        "N" : "-.",
+        "O" : "---",
+        "P" : ".--.",
+        "Q" : "--.-",
+        "R" : ".-.",
+        "S" : "...",
+        "T" : "-",
+        "U" : "..-",
+        "V" : "...-",
+        "W" : ".--",
+        "X" : "-..-",
+        "Y" : "-.--",
+        "Z" : "--..",
+        " " : "/",
+        "1" : ".----",
+        "2" : "..---",
+        "3" : "...--",
+        "4" : "....-",
+        "5" : ".....",
+        "6" : "-....",
+        "7" : "--...",
+        "8" : "---..",
+        "9" : "----.",
+        "0" : "-----",
+        "." : ".-.-.-",
+        "," : "--..--",
+        ":" : "---...",
+        "?" : "..--..",
+        "'" : ".----.",
+        "-" : "-....-",
+        "@" : ".--.-.",
+        "=" : "-...-"
+        }
+
+
+
+inverse_morse_alphabets = dict((v,k) for (k,v) in morse_alphabets.items())
+
+class MorseEncodeDecode:
+    
+    def decodeFromMorse(self, code, position_in_string = 0):
+        
+        if position_in_string < len(code):
+            morse_letter = ""
+            for key,char in enumerate(code[position_in_string:]):
+                if char == " ":
+                    position_in_string = key + position_in_string + 1
+                    letter = inverse_morse_alphabets[morse_letter]
+                    return letter + decodeFromMorse(code, position_in_string)
+                
+                else:
+                    morse_letter += char
+        else:
+            return ""
+    
+    
+    def encodeToMorse(self, message):
+        encoded_message = ""
+        for char in message[:]:
+            encoded_message += morse_alphabets[char.upper()] + " "
+                
+        return encoded_message
