aae439c73d0174fe710aa55edf5998cb46a98e01
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-10-13 15:06:23 +0300

    Added console app (.net core) for CountSingleDigits solution

---
+++ C#/CountSingleDigits/CountSingleDigits/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSingleDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountSingleDigit(99995844)); // Return 2 (only 5 and 8 appear once)
+        }
+        static int CountSingleDigit(long input)
+        {
+            var digitString = input.ToString(); // change to string input value.
+            var allDigits = new List<char>();   // add all digits in char list
+            foreach (var digit in digitString)
+            {
+                allDigits.Add(digit);
+            }
+            var result = 0;
+            var distinctDigits = allDigits.Distinct(); // find different digits list
+            foreach (var distinctDigit in distinctDigits)
+            {
+                if (allDigits.Count(m => m == distinctDigit) == 1) // check different digits count
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
