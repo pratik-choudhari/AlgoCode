5f9eb16868dd9cf3f16567f98dfef85c63282e7f
Uddeshya Tyagi <uddeshyatyagi775@gmail.com>
2020-10-13 22:17:09 +0530

    Added clock sound

--- Python/Countdown Timer/countdown1.py
+++ Python/Countdown Timer/countdown1.py
@@ -1,5 +1,6 @@
 # import the time module 
 import time 
+import pygame
 
 # define the countdown func. 
 def countdown(t): 
@@ -9,9 +10,13 @@ def countdown(t):
         #divmod functions return quotient and remainder
         mins, secs = divmod(t, 60) 
         
-        #:02d repesents that minutes and seconds will be represented in 2 digits
+        #:02d repesents that minutes and seconds will be 		represented in 2 digits
         timer = '{:02d}:{:02d}'.format(mins, secs) 
-
+        pygame.mixer.init()
+        pygame.mixer.music.load('clock.ogg')#any mp3/ogg file
+        pygame.mixer.music.play()
+       #while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick()
         print(timer, end="\r") 
         time.sleep(1) 
         t -= 1
@@ -23,4 +28,5 @@ t = input("For how many seconds do you want to set the timer? ")
 
 # function call 
 countdown(int(t)) 
+
 
