886dc3c5e934108c25b04700d8d43c1ef978e60b
subhadeep1912 <subhadeepc19@gmail.com>
2020-10-14 01:59:19 +0530

    Added maximum number of handshakes in python

---
+++ Python/Maximum_handshakes/maximum_handshakes.py
@@ -0,0 +1,5 @@
+num_people = int(input("Enter the number of people: "))
+num_handshakes = num_people * (num_people-1)/2
+print("The number of handshakes will be: ")
+print(num_handshakes)
+#Python is the easiest to understand
