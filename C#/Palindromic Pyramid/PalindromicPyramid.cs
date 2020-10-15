30bb527463241a1722c1e9c34d8e6370b671832a
Batuhan Görkem Benzer <72701643+bgbenzer@users.noreply.github.com>
2020-10-15 16:29:54 +0300

    Update PalindromicPyramid.cs

--- C#/Palindromic Pyramid/PalindromicPyramid.cs
+++ C#/Palindromic Pyramid/PalindromicPyramid.cs
@@ -6,22 +6,25 @@ public class Program
 	{
 		
 		int row;
-		int num = 1 ;
-
 		Console.Write("Please enter a integer :");
 		
 		try{
 			
 			row = Int16.Parse(Console.ReadLine());
-			
-			for (int i = 0; i < row+1; i++) {
-				for (int j = 0; j < i; j++) {
-					Console.Write(num + " ");
-					num = num+1 ;
+
+			for (int i = 1; i < row+1; i++) {
+				for(int j = 1; j <= (i); j++){
+					Console.Write(j + " ");
+				}	
+				for(int k = (i-1); k >= 1; k--){
+					Console.Write(k + " ");
+
 				}
 				Console.WriteLine("");
 				
 			}
+			
+			
 		}	
 		
 		catch (Exception e){
