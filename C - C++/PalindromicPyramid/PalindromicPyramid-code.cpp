38b2232af7c394c90d33dd01db32a98ea4eb496a
Anshika Bhatt <anshikabhatt0207@gmail.com>
2020-10-17 09:59:42 +0530

    Add PalindromicPyramid-C++

--- C - C++/PalindromicPyramid/PalindromicPyramid-code.cpp
+++ C - C++/PalindromicPyramid/PalindromicPyramid-code.cpp
@@ -8,6 +8,7 @@ int main()
     cout<<"Enter number of row:\n";
     cin>>n;
     int i,j,k;
+    //here i represents the row	
     for(int i=0;i<n;i++){
         for(int j=1;j<=i+1;j++){
             cout<<j;
